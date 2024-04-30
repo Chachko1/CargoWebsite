f70963615286aedaeff972773ac354ef8bd9185f
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-04-30 18:45:46 +0300

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,12 +4,11 @@ const Header = () => {
   return (
   <section className="flex flex-col items-center justify-center">
             <h1 className="text-customBlue font-inter font-semibold text-2xl text-center w-[80%]
-            sm:text-4xl md-mt-[70px] md:leading-[108%] md:-tracking-[4px] lg:w-[50%] lg:text-5xl xl:text-6xl
-            2xl:text-7xl lg:pt-10">
+            sm:text-4xl md-mt-[70px] md:leading-[108%] md:-tracking-[4px] lg:w-[50%] lg:text-5xl lg:pt-10">
                 Гъвкави решения за сухопътен транспорт за всеки клиент
             </h1>
         <div className="mt-5 max-w-[90%] lg:mt-[60px]">
-            <img src="image.png" alt="" width={969} height={690} />
+            <img src="truck.png" alt="" width={969} height={690} />
         </div>
     
         </section>
